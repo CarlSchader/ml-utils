f0730585a482ab32592a07cd9fe97205a245e5b8
carlschader <carlschader@gmail.com>
2024-03-31 19:20:41 -0600

    loss function works. It may be sped up, not sute

--- carlschader_ml_utils/contrastive/train.py
+++ carlschader_ml_utils/contrastive/train.py
@@ -1,26 +1,18 @@
 import torch
-import torch.nn as nn
 
-def simCLR_criterion(outputs, temperature=0.1):
-    batch_size = outputs.size(0)
+def simCLR_criterion(batch1, batch2, temp=0.1):
+    batch_size = batch1.size(0)
+    norms = torch.cat([batch1, batch2], dim=0)
+    norms = norms / norms.norm(dim=1)[:, None]
+    sims = torch.mm(norms, norms.t())
+    exps = torch.exp(sims / temp)
+    sums = exps.sum(dim=0)
+    numerators = torch.cat((torch.diagonal(exps, offset=batch_size, dim1=1, dim2=0), torch.diagonal(exps, offset=batch_size, dim1=0, dim2=1)))
+    denominators = sums - numerators
+    neg_logs = -torch.log(numerators / denominators)
+    return neg_logs.mean()
 
-    # Calculate cosine similarity
-    outputs = outputs / outputs.norm(dim=1)[:, None]
-    sim = torch.mm(outputs, outputs.t())
-
-    # Calculate the loss
-    mask = torch.eye(batch_size, dtype=torch.bool).to(outputs.device)
-    neg_mask = ~mask
-
-    pos = sim[mask].view(batch_size, 1)
-    neg = sim[neg_mask].view(batch_size, -1)
-
-    logits = torch.cat([pos, neg], dim=1)
-    logits /= temperature
-
-    labels = torch.zeros(batch_size, dtype=torch.long).to(outputs.device)
-    return nn.CrossEntropyLoss()(logits, labels)
-
+    
 def simCLR_train(model, train_loader, optimizer, criterion, device, verbose=False):
     model.train()
     total_loss = 0
@@ -45,3 +37,25 @@ def simCLR_train(model, train_loader, optimizer, criterion, device, verbose=Fals
             print(f'Processed batch... {batches}/{total_batches}', end='\r')
 
     return total_loss / total_batches
+
+if __name__ == '__main__':
+    import time
+    start = time.time()
+    batch_size = 10000
+    temp = 0.1
+    
+    b1 = torch.stack([torch.arange(start=(i*batch_size)+1, end=((i+1)*batch_size)+1) for i in range(batch_size)])
+    b2 = (0.5 * b1) + 3
+
+    print(simCLR_criterion(b1, b2, temp))
+    # print(simCLR_criterion(torch.tensor([
+    #     [1.0, 0.0, 1.0],
+    #     [-0.5, 0.866, 0.0],
+    #     [-0.5, -0.866, 0.0],
+    # ]), torch.tensor([
+    #     [1.0, 0.0, 0.0],
+    #     [-0.5, 0.866, 0.0],
+    #     [-0.5, -0.866, 0.0],
+    # ]), temp))
+
+    print('Time:', time.time() - start)
