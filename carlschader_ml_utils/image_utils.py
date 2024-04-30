fixed off by one error in embed image dataset function

carlschader_ml_utils/image_utils.py
@@ -84,7 +84,6 @@ def embed_image_folder(
                             os.mkdir(os.path.join(save_dir, class_name))
                         for k in range(class_stack.shape[0]):
                             torch.save(class_stack[k], os.path.join(save_dir, class_name, f'{k}.pth'))
-                        # torch.save(class_stack, os.path.join(save_dir, f'{class_name}.pth'))
                         class_stack = []
                         current_count = 0
                         current_label = label
@@ -95,6 +94,19 @@ def embed_image_folder(
             batches += 1
             if verbose:
                 print(f'Processed batch... {batches}/{total_batches}', end='\r')
+
+        # save last class
+        if averages_only:
+            class_name = dataset.classes[current_label]
+            class_average = class_average / current_count
+            torch.save(class_average, os.path.join(save_dir, class_name, 'average.pth'))
+        else:
+            class_name = dataset.classes[current_label]
+            class_stack = torch.stack(class_stack, dim=0)
+            if not os.path.exists(os.path.join(save_dir, class_name)):
+                os.mkdir(os.path.join(save_dir, class_name))
+            for k in range(class_stack.shape[0]):
+                torch.save(class_stack[k], os.path.join(save_dir, class_name, f'{k}.pth'))
 
 def find_image_folder_normalization(path, crop_size=224, batch_size=64, total_batches=None, device=torch.device('cpu'), verbose=False):
     transform = transforms.Compose([

scripts/eval_distance_embedding_dataset.py
@@ -7,9 +7,10 @@ device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cp
 parser = argparse.ArgumentParser(description='Given an evaluation dataset folder of embeddings, classify them using euclidean distance against another dataset folder of embeddings.')
 parser.add_argument('-e' ,'--embedding_eval_set_path', required=True, type=str, help='Path to the folder of embeddings')
 parser.add_argument('-d', '--embedding_dataset_path', required=True, type=str, help='Path to the image to classify')
-parser.add_argument('-t', '--test-batch-size', type=int, default=8, help='Batch size for the test set')
+parser.add_argument('-t', '--test-batch-size', type=int, default=32, help='Batch size for the test set')
 parser.add_argument('-b', '--batch-size', type=int, default=32, help='Batch size for the dataset')
 parser.add_argument('-m', '--metric', type=str, default='euclidean', help='Distance metric to use')
+parser.add_argument('-o', '--output', type=str, default='output.txt', help='Output file to write results to')
 args = parser.parse_args()
 
 dataset_path = args.embedding_dataset_path